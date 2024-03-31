51df8b6d83af5f0f945dea4cc69d821cea028c90
camila.pereyra <campereyra23051995@gmail.com>
2024-03-31 17:07:30 -0300

    boton de reseteo, logica de empate

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,70 @@
 import { useState } from "react";
 import "./App.css";
 import Cuadrado from "./components/Cuadrado/Cuadrado";
-//turnos del juego
+
 const TURNOS = {
   X: "x",
   O: "o",
 };
 
+const POSICIONES_GANADORAS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function App() {
   const [tablero, setTablero] = useState(Array(9).fill(null));
   const [turno, setTurno] = useState(TURNOS.X);
+  const [ganador, setGanador] = useState(false);
+
+  const chequearGanador = (tableroAChequear) => {
+    for (const posicion of POSICIONES_GANADORAS) {
+      const [a, b, c] = posicion;
+      if (
+        tableroAChequear[a] &&
+        tableroAChequear[a] === tableroAChequear[b] &&
+        tableroAChequear[a] === tableroAChequear[c]
+      ) {
+        return tableroAChequear[a];
+      }
+    }
+    return ganador;
+  };
+
   const actualizarTablero = (posicion) => {
-    //actualizamos el array que contiene el estado del tablero
-    const nuevoTablero = [...tablero];
-    nuevoTablero[posicion] = turno;
-    setTablero(nuevoTablero);
-    //actualizamos el turno
-    turno === TURNOS.X ? setTurno(TURNOS.O) : setTurno(TURNOS.X);
+    if (tablero[posicion] || ganador !== false) {
+      return;
+    } else {
+      //actualizamos el array que contiene el estado del tablero
+      const nuevoTablero = [...tablero];
+      nuevoTablero[posicion] = turno;
+      setTablero(nuevoTablero);
+      //cuando seteamos el tablero hay que chequear si hay un ganador
+      const verificacionGanador = chequearGanador(nuevoTablero);
+      //si hay un ganador
+      if (verificacionGanador !== false) {
+        setGanador(verificacionGanador);
+      }
+      //si no hay ganador
+      //chequeamos que no haya empate
+      else if (!nuevoTablero.includes(null)) {
+        setGanador(null);
+      } else {
+        turno === TURNOS.X ? setTurno(TURNOS.O) : setTurno(TURNOS.X);
+      }
+    }
+  };
+
+  const resetearJuego = () => {
+    setTablero(Array(9).fill(null));
+    setTurno(TURNOS.X);
+    setGanador(false);
   };
   return (
     <>
@@ -35,8 +83,19 @@ function App() {
         </div>
       </section>
       <section className="turno">
-        <span>Turno de: {turno}</span>
+        {ganador === false && <span>Turno de: {turno}</span>}
+        {ganador === null && <span>Empate</span>}
+        {ganador !== false && ganador !== null && (
+          <span>Ganador del juego: {turno}</span>
+        )}
       </section>
+      {(ganador !== false || ganador === null) && (
+        <section className="reseteo">
+          <button className="buttonResetear" onClick={resetearJuego}>
+            Resetear juego
+          </button>
+        </section>
+      )}
     </>
   );
 }
--- src/components/Cuadrado/Cuadrado.jsx
+++ src/components/Cuadrado/Cuadrado.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
 import "./Cuadrado.css";
 
 // eslint-disable-next-line react/prop-types
 const Cuadrado = ({ index, children, actualizarTablero }) => {
-  const [isClicked, setIsClicked] = useState(false);
   const handleClick = () => {
-    if (isClicked === false) {
-      setIsClicked(true);
-      actualizarTablero(index);
-    }
+    actualizarTablero(index);
   };
 
   return (
